f3a4ea34e7a990b525a260c1128ab8a41ed1bc71
Mahender Sharma <72732952+mahendersharma@users.noreply.github.com>
2022-09-03 18:23:38 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,7 @@
+function funbutton(){
+    document.getElementById('fun').innerHTML='Hello mahender'
+    document.getElementById('fun').style.color= 'red'
+
+    document.getElementById('fun1').innerHTML='Hello '
+
+}
